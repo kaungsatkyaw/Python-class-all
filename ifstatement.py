2fd51f638a8332ed4c6ee5b8fb1586f28b463147
kaungsatkyaw <fired6559@gmail.com>
2019-11-03 17:44:43 +0630

    ifstatement

---
+++ ifstatement.py
@@ -0,0 +1,77 @@
+#  x = int(input("Please enter your integer:"))
+
+#  if x < 0:
+#  	x = 0
+#  	print('Negative changed to zero')
+#  elif x == 0:
+#  	print('zero')
+#  elif x == 1:
+#  	print('single')
+#  else:
+#  	print('More')
+
+#  int - integer(Number)
+#  str - string(charcter)
+#  bool - true,false
+#  float - 0.02,0.4,0.2
+#  ***ctrl + ? to close line***
+
+#  if x < 5:
+#  	print('x is smaller than 5')
+#  elif x == 7:
+#  	print('x is equal to 7')
+#  elif x > 5 and x < 10:
+#  	print('x is a number between 5 and 10')
+#  elif x > 10:
+#  	print('x is bigger than 10')
+#  else:
+#  	print('x is nothing')
+#  if x <= 0:
+#  	print('R u kidding me?')
+#  elif x >= 1 and x <= 18:
+#  	print('go to school')
+#  elif x >= 19 and x <= 50:
+#  	print('go to beach')
+#  elif x >= 51 and x <= 75:
+#  	print('go to pagoda')
+#  else:
+#	print('if u wish,you can die.')
+
+#  print("I told that \"I don\'t like it\"")
+# words = ['cat', 'window', 'defenestrate']
+# for w in words:
+# 	print(w,len(w))
+
+# fruit = ['apple', 'banana', 'cucumber', 'pineapple', 'orange']
+# for f in fruit:
+# 	print(f,len(f))
+
+# for i in range(9):
+#  	print(i)
+
+# for x in range(0,10):
+# 	print(x)
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+# for i in range(len(a)):
+# 	print(i, a[i])
+
+# a = ['1', '2', '3', '4', '5']
+# for i in range(len(a)):
+#  	print(i, a[i]) 
+
+# print(sum(range(4)))
+
+# print(list(range(4)))
+
+# f=['a', 'b']
+# f.append('c')
+# print(f)
+
+# f=['a', 'b']
+# f.remove('b')
+# print(f)
+
+print(a.count('Mary'))
+print(a.index('Mary'))
+print(a.index('Mary',0))
