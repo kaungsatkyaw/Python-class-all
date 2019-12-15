6f53481043884fa4ec8d29bcd18b0cf4be41dd95
kaungsatkyaw <fired6559@gmail.com>
2019-12-15 17:34:37 +0630

    module

---
+++ moduletest.py
@@ -0,0 +1,15 @@
+import mymodule
+
+x = mymodule.student["code"]
+a = mymodule.student["name"]
+y = mymodule.student1["mother"]
+b = mymodule.student1["father"]
+z = mymodule.student2["age"]
+c = mymodule.student2["class"]
+
+print(x)
+print(a)
+print(y)
+print(b)
+print(z)
+print(c)
